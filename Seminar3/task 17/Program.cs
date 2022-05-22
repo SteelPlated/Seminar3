3fdba1063468c6e6bc6e23ff633ea7d0ee28a562
SteelPlated <mix.singatulin@yandex.ru>
2022-05-22 11:19:15 +0500

    task 17

---
+++ Seminar3/task 17/Program.cs
@@ -0,0 +1,27 @@
+﻿/*17. Напишите программу, которая принимает на вход
+координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+номер четверти плоскости, в которой находится эта
+точка.*/
+
+Console.WriteLine("Введите координату x: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату y: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Диапазон находится в 1 четверти");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("Диапазон находится в 2 четверти");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("Диапазон находится в 3 четверти");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("Диапазон находится в 4 четверти");
+}
